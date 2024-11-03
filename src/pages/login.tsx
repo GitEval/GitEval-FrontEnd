de996266ea7bfffa78ff449293eb2633558a5cbb
eleliauk <2831336720@qq.com>
2024-11-03 11:19:16 +0800

    ✨ feat：新增登录静态

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,36 @@
+import GithubIcon from '@/components/common/githubIcon';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 function Login() {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div className="flex items-center justify-center w-full min-h-screen p-6">
+      <div className="grid gap-6 w-full max-w-sm">
+        <h1 className="text-3xl font-bold">登录</h1>
+        <Card>
+          <CardHeader>
+            <CardTitle>GitEval</CardTitle>
+            <CardDescription>描述描述描述描述</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>内容内容内容内容内容</p>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full inline-flex gap-4">
+              <GithubIcon className="h-4 w-4" />
+              GitHub
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
   );
 }
 export default Login;
